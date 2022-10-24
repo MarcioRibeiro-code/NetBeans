0da4efea77d93f98acdaaad1d940431a917dc95b
insosso <74792561+insosso@users.noreply.github.com>
2022-10-24 11:32:32 +0100

    24-10-2022

---
+++ CS50/readability/readability.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int calc_index(char *string);
+
+int main(void) 
+{
+
+
+
+}
+
+int calc_index (char *string){
+
+    int L,S;
+
+
+for(int i=0;i<strlen(string);i++){
+
+}
+
+
+}
